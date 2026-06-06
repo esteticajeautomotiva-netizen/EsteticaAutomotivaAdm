593cad5909f2554063e6d3c037c8dc01fb7721c7
esteticajeautomotiva-netizen <esteticajeautomotiva@gmail.com>
2026-06-06 13:42:41 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'je-adm-v6';
+const CACHE_NAME = 'je-adm-v7';
 const BASE = '/EsteticaAutomotivaAdm';
 const ASSETS = [
   `${BASE}/index.html`, `${BASE}/login.html`, `${BASE}/admin.html`,
